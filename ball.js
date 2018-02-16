51e62b4d95f1dca11d051c674e50b2a55f4a9344
NickSklA <NickSklA@users.noreply.github.com>
2018-02-16 11:53:17 +0200

    Update ball.js

--- ball.js
+++ ball.js
@@ -3,37 +3,102 @@ function Ball(){
 	this.x = width/2;
 	this.y = height/2;
 
-	var angle = random(TWO_PI);  // generate random angle [0 , 360] 
+	this.diameter = 32;
 
-	this.xspeed = 8 * cos(angle);
-	this.yspeed = 8 * sin(angle);
+	this.xspeed = 7; 
+	this.yspeed = 6;
 
 
 	this.show = function(){
 
-		fill('red');
-		ellipse(this.x , this.y , 40 , 40);
-	}
 
+		fill(40, 184, 184);
+		ellipse(this.x , this.y , this.diameter , this.diameter);
+		//textSize(20);
+		//fill(40, 184, 184);
+		//text("(" + this.x + " ," + this.y + ")" , this.x + 10 , this.y - 10);
+	}
 
 	this.update = function(){
 
 		this.x += this.xspeed;
 		this.y += this.yspeed;
-		this.yspeed = this.bounce(this.y , this.yspeed , 0 , windowHeight);
-		this.xspeed = this.bounce(this.x , this.xspeed , 0 , windowWidth);
-		
 
+		this.bounce();
 	}
 
 
-	this.bounce = function(position , speed , min , max){
 
 
-		if(position > max || position < min){
-			speed *= -1;
+
+	this.bounce = function(){
+
+
+		// player1 score 
+
+		if ( this.x >= width - 16){
+			this.xspeed = 5; 
+			this.yspeed = 4;
+			player1.resetPlayer();
+			player2.resetPlayer();	
+			leftScore += 1;
+			this.x = width/2;
+			this.y = height/2;
+			this.xspeed *= -1;
 		}
-		return speed;
+
+
+		// player2 score
+
+		if(this.x <= 16){
+			this.xspeed = 5; 
+			this.yspeed = 4;
+			player1.resetPlayer();
+			player2.resetPlayer();		
+			rightScore += 1;
+			this.x = width/2;
+			this.y = height/2;
+			this.xspeed *= -1;
+		}
+
+		if(this.y >= height - 16  || this.y <= 16){
+
+			this.yspeed *= -1;
+
+		}
+
+
+
+		// player 1
+
+		if (this.x <= player1.x + this.diameter/2 + player1.playerWidth && this.y <= player1.y + player1.ylength && this.y >= player1.y){
+
+
+				this.xspeed *= -1;
+			
+
+		}
+
+
+		//player 2
+
+		if (this.x >= player2.x - this.diameter/2  && this.y <= player2.y + player2.ylength && this.y >= player2.y){
+
+			
+				this.xspeed *= -1;
+			
+
+		}
+
 	}
+	
+    this.totalReset = function() {
+        leftScore = 0;
+        rightScore = 0;
+        this.x = width/2;
+        this.y = height/2;
+        this.xspeed = 0;
+        this.yspeed = 0;
+    }
 
 }
